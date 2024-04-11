c5601af0f849c12c1a51454dc8c5374568a1a284
Vitória Cardoso <123764373+vitoriaccardoso@users.noreply.github.com>
2024-04-11 17:28:28 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,55 +1,99 @@
-'use strict'
-import {getFilmes} from "./filmes.js"
-
-console.table(await getFilmes())
-
-function criarCard (filme){
+import { getFilmes, getFilme, postFilme } from "./filmes.js"
+// console.log(getFilmes)
+function criarCard(filme) {
+    
+    
     const card = document.createElement('div')
-    card.classList.add('flex')
-    card.classList.add('flex-col')
-    card.classList.add('text-[#ECDDA2]')
-    card.classList.add('justify-center')
-    card.classList.add('cursor-pointer')
+    const id = filme.id_filme
+    console.log(filme)
+    card.addEventListener("click", () => {abrirFilme(id)
+    console.log(id)})
     const titulo = document.createElement('h2')
+    const valor = document.createElement('button')
+    const capa_filme = document.createElement('img')
+    valor.textContent = filme.valor_unitario
     titulo.textContent = filme.nome
-    titulo.classList.add('bg-[#7A7A7A]')
-    titulo.classList.add('font-bold')
-    titulo.classList.add('text-center')
-    titulo.classList.add('mr-1')
-    titulo.classList.add('w-64')
-    titulo.classList.add('border-b-4')
-    titulo.classList.add('border-x-4')
-    titulo.classList.add('border-[#ECDDA2]')
-    const texto = document.createElement('p')
-    texto.textContent = filme.sinopse
-    texto.classList.add('w-72')
-    const preco = document.createElement('p')
-    preco.textContent = filme.valor_unitario
-    const capa = document.createElement('img')
-    capa.src = filme.foto_capa
-    capa.classList.add('h-92')
-    capa.classList.add('w-64')
-    capa.classList.add('border-t-4')
-    capa.classList.add('border-x-4')
-    capa.classList.add('border-[#ECDDA2]')
-    const dataLancamento = document.createElement('p')
-    dataLancamento.textContent = filme.data_lancamento
-    card.append(capa, titulo)
-    card.addEventListener('click',()=> {
-        window.location.href='../sobre.html?id='+filme.id
-    })
+    capa_filme.src = filme.foto_capa
+ 
+    capa_filme.classList.add(
+        'h-56',
+        'mx-px',
+        'rounded-md',
+        'border-blue-800',
+        'flex',
+        'flex-row'
+        ) 
+        card.append(capa_filme, titulo)
+        
+        return card
+    }
+    
+    async function abrirFilme(idFilme){
+        const filme = await getFilme(idFilme)
+        console.log(filme)
+        focoFilme(filme)
+        
+    }
+    
+    async function preencherContainer(){
+        
+        const container = document.querySelector('body')
+        const filmes = await getFilmes()
+        
+        filmes.forEach(filme => {
+            const main = criarCard(filme)
+            container.appendChild(main)
+        })
+        
+        
+        
+    }
+    preencherContainer()
+    
+    
+    const divFundo = document.getElementById('fundo')
+    async function focoFilme(filme){
+        divFundo.classList.remove('hidden')
+        
+        const header = document.createElement('header')
+        const cabecalho = document.createElement('div')
+        const capa_filme = document.createElement('img')
+        capa_filme.src = filme.capa_filme
+        
+        const titulo = document.createElement('h1')
+        titulo.textContent = filme.nome
+        
+        const sinopse = document.createElement('h1')
+        sinopse.textContent = document.sinopse
+        
+        const duracao = document.createElement('h1')
+        duracao.textContent = document.duracao
+        
+        const dataLancamento = document.createElement('h1')
+        dataLancamento.textContent = document.dataLancamento
+        
+        const dataRelancamento = document.createElement('h1')
+        dataRelancamento.textContent = document.dataRelancamento
+        
+        const linkFotoCapa = document.createElement('h1')
+        linkFotoCapa.textContent = document.linkFotoCapa
+        
+        const valor = document.createElement('h1')
+        valor.textContent = document.valor
+        
+        const geral = document.createElement('div')
+        geral.textContent = document.geral
+        
+        const espaco = document.createElement('br')
+        
+        const divTexto = document.createElement('div')
+
+  divTexto.replaceChildren(titulo, sinopse, duracao, dataLancamento, dataRelancamento)
+  cabecalho.replaceChildren(capa_filme)
+  header.replaceChildren(cabecalho)
+  
+  console.log('teste')
+  
+
 }
 
-async function preencherContainer(){
-    const container = document.querySelector('body')
-
-    const filmes = await getFilmes()
-
-    filmes.forEach(filme => {
-        criarCard(filme)
-    })
-}
-
-
-
-preencherContainer()
